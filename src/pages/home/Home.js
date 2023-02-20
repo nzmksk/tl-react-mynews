1bff21b64d5c3ae2252de5181460312ffca0da20
Mohd Hafiz Zabba <mhafiz9676@gmail.com>
2023-02-21 00:09:35 +0800

    Update Home.js
    Add Header component into homepage.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,25 @@
+import { Grid } from "@mui/material";
+import Header from "./components/Header";
+
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <Grid container className="main-container" direction={"column"}>
+      <Grid
+        className="header-container"
+        item
+        lg={1}
+        style={{ maxHeight: "10vh" }}
+      >
+        <Header />
+      </Grid>
+      <Grid className="content-container" item lg={11}>
+        <Grid container direction={"row"} style={{ height: "100%" }}>
+          <Grid className="left-panel-container" item lg={2.5}></Grid>
+          <Grid className="result-container" item lg={9.5}></Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
 
-export default Home
+export default Home;
