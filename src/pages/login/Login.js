1010920561f12190f1d922c05f888d4c697768b3
nzmksk <mhafiz9676@gmail.com>
2023-04-17 03:22:30 +0800

    Add loading bar to Login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,6 +2,7 @@ import {
   Alert,
   Box,
   Button,
+  LinearProgress,
   Slide,
   Snackbar,
   TextField,
@@ -23,6 +24,7 @@ function Login() {
   const [isLoggedIn, setIsLoggedIn] = useState(
     JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY.loginStatus)) ?? false
   );
+  const [isLoginInProgress, setIsLoginInProgress] = useState(false);
   const [openMessage, setOpenMessage] = useState(false);
   const [errorMessage, setErrorMessage] = useState({
     message: "",
@@ -41,6 +43,7 @@ function Login() {
   // Form submission
   const handleSubmit = (event) => {
     event.preventDefault();
+    setIsLoginInProgress(true);
     const { username, password } = loginForm;
 
     // If field is empty
@@ -50,27 +53,32 @@ function Login() {
         message: "Fields cannot be empty.",
         severity: "error",
       });
-    }
-    // If login credential is valid
-    else if (username === "John" && password === "12345") {
-      setIsLoggedIn(!isLoggedIn);
-      localStorage.setItem(
-        LOCAL_STORAGE_KEY.username,
-        JSON.stringify(username)
-      );
-      localStorage.setItem(
-        LOCAL_STORAGE_KEY.loginStatus,
-        JSON.stringify(!isLoggedIn)
-      );
-      navigate("/home");
-    }
-    // If login credential is invalid
-    else {
-      setOpenMessage(true);
-      setErrorMessage({
-        message: "Username and password do not match.",
-        severity: "warning",
-      });
+      setIsLoginInProgress(false)
+    } else {
+      setTimeout(() => {
+        // If login credential is valid
+        if (username === "John" && password === "12345") {
+          setIsLoggedIn(!isLoggedIn);
+          localStorage.setItem(
+            LOCAL_STORAGE_KEY.username,
+            JSON.stringify(username)
+          );
+          localStorage.setItem(
+            LOCAL_STORAGE_KEY.loginStatus,
+            JSON.stringify(!isLoggedIn)
+          );
+          navigate("/home");
+        }
+        // If login credential is invalid
+        else {
+          setOpenMessage(true);
+          setErrorMessage({
+            message: "Username and password do not match.",
+            severity: "warning",
+          });
+        }
+        setIsLoginInProgress(false);
+      }, 1000);
     }
   };
 
@@ -91,67 +99,70 @@ function Login() {
   // Otherwise, show the Login page
   else {
     return (
-      <Box
-        component="form"
-        method="post"
-        onSubmit={handleSubmit}
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        margin="auto"
-        mt={5}
-        maxWidth={500}
-        borderRadius={5}
-        boxShadow="5px 5px 10px #ccc"
-      >
-        <Typography variant="h3" padding={2}>
-          MyNews
-        </Typography>
-        <TextField
-          label="Username"
-          name="username"
-          type="text"
-          variant="outlined"
-          size="small"
-          margin="dense"
-          value={loginForm.username}
-          onChange={handleFieldChange}
-        />
-        <TextField
-          label="Password"
-          name="password"
-          type="password"
-          variant="outlined"
-          size="small"
-          margin="dense"
-          value={loginForm.password}
-          onChange={handleFieldChange}
-        />
-        <Button
-          variant="contained"
-          size="medium"
-          type="submit"
-          sx={{ margin: "20px" }}
+      <>
+        {isLoginInProgress && <LinearProgress />}
+        <Box
+          component="form"
+          method="post"
+          onSubmit={handleSubmit}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          margin="auto"
+          mt={5}
+          maxWidth={500}
+          borderRadius={5}
+          boxShadow="5px 5px 10px #ccc"
         >
-          Login
-        </Button>
-        <Snackbar
-          anchorOrigin={{ vertical: "top", horizontal: "center" }}
-          open={openMessage}
-          autoHideDuration={5000}
-          onClose={handleClose}
-          TransitionComponent={SlideTransition}
-        >
-          <Alert
-            onClose={handleClose}
-            severity={errorMessage.severity}
-            sx={{ width: "100%" }}
+          <Typography variant="h3" padding={2}>
+            MyNews
+          </Typography>
+          <TextField
+            label="Username"
+            name="username"
+            type="text"
+            variant="outlined"
+            size="small"
+            margin="dense"
+            value={loginForm.username}
+            onChange={handleFieldChange}
+          />
+          <TextField
+            label="Password"
+            name="password"
+            type="password"
+            variant="outlined"
+            size="small"
+            margin="dense"
+            value={loginForm.password}
+            onChange={handleFieldChange}
+          />
+          <Button
+            variant="contained"
+            size="medium"
+            type="submit"
+            sx={{ margin: "20px" }}
           >
-            {errorMessage.message}
-          </Alert>
-        </Snackbar>
-      </Box>
+            Login
+          </Button>
+          <Snackbar
+            anchorOrigin={{ vertical: "top", horizontal: "center" }}
+            open={openMessage}
+            autoHideDuration={5000}
+            onClose={handleClose}
+            TransitionComponent={SlideTransition}
+          >
+            <Alert
+              onClose={handleClose}
+              severity={errorMessage.severity}
+              sx={{ width: "100%" }}
+            >
+              {errorMessage.message}
+            </Alert>
+          </Snackbar>
+        </Box>
+      </>
     );
   }
 }
